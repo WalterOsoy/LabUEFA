89e1ae8dd1b7574a88e0c0e5c9e887b6306c5299
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2019-04-18 22:30:37 -0600

    First Commit

---
+++ Laboratorio5_UEFA/Laboratorio5_UEFA/Models/Album.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio5_UEFA.Models
+{
+    public class Album
+    {
+        public string tipo { get; set; }
+        public List<carta> numero = new List<carta>();
+        public void insertarNumero(int num)
+        {
+            carta temp = new carta(num);
+            if (!BuscarCarta(num))
+            {
+                numero.Add(temp);
+            }
+        }
+        public bool BuscarCarta(int num)
+        {
+            List<carta> finded = numero.FindAll(x => x.numero.Equals(num));
+            if (finded.Count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class carta : IComparable
+    {
+        public int numero { get; set; }
+        public carta() { }
+        public carta(int num)
+        {
+            this.numero = num;
+        }
+        public int CompareTo(object obj)
+        {
+            carta tempo = new carta();
+            return this.numero.CompareTo(tempo.numero);
+        }
+    }
+}
---
+++ Laboratorio5_UEFA/Laboratorio5_UEFA/Models/Coleccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio5_UEFA.Models
+{
+    public class Coleccion
+    {
+        public string tipo { get; set; }
+        public List<carta> Disponibles = new List<carta>();
+        public List<carta> intercambios = new List<carta>();
+
+    }
+
+}
